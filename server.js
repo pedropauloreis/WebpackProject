f439d386af77d5f8f810decff5bc8ff7aedcb6eb
Pedro Paulo de Oliveira Reis <ppreis@gmail.com>
2021-07-22 14:09:16 +0200

    preparing for AWS EB

--- server.js
+++ server.js
@@ -8,19 +8,16 @@ app.get('/hello', (req,res) => res.send({hi:'there'}));
 
 console.log(`Running in ${process.env.NODE_ENV} environment`);
 
-if(process.env.NODE_ENV ==='development') {
-    
-    app.use(webpackMiddleware(webpack(webpackConfig)));
-    app.use(express.static('dist'));
-    app.get('*', (req,res) => {
-        res.sendFile(path.join(__dirname, 'dist/index.html'));
-    })
-
-} else {
+if(process.env.NODE_ENV !=='production') {
     
     const webpackMiddleware = require('webpack-dev-middleware');
     const webpack = require('webpack');
     const webpackConfig = require('./webpack.config.js');
-    
+    app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+    app.use(express.static('dist'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+    })
 }
 app.listen(process.env.PORT || 3050, () => console.log('Listening on port 3050'));
